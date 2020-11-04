1b125bdc625ca58761519d1e11cb999e3f9bea0d
Kwanyoung Christopher Kim <kwanyoung.kim@patelhs.org>
2020-11-04 17:55:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("\n");
+    System.out.println("It's the first day of school in Ms.Benny's class and she needs your help assigning seats to her students.\n");
+
+  }
+}
