e9e7ba1f415eafcddcb87b330cb8a6cad8bb3430
Kwanyoung Christopher Kim <kwanyoung.kim@patelhs.org>
2020-11-04 17:59:51 +0000

    Declared values and a error prevention system

--- Main.java
+++ Main.java
@@ -6,5 +6,15 @@ class Main {
     System.out.println("\n");
     System.out.println("It's the first day of school in Ms.Benny's class and she needs your help assigning seats to her students.\n");
 
+    Scanner getScanner = new Scanner(System.in);
+
+    int[] Seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    String[] Names = new String[15];
+
+    if (Names.length > Seats.length) {
+      System.out.println("Uh oh, it looks like there is not enough seats for this class! Try adding more seats!");
+      return;
+    }
+
   }
 }
